d14a8d1605f2e70f28c1f61c54a41e8ab1edc0ba
unknown <lee.le@VNSGNICT023.itl.com>
2015-12-15 16:31:31 +0700

    advance search

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -8,23 +8,42 @@ $(document).ready(function() {
 	$(function () {
     $('.date-picker').datetimepicker();
   });
+  //advance-search
+  $(function () {
+    $( '#show-advance-search-btn').click(function(e) {
+    	e.preventDefault();
+    	if ($('#advance-search').hasClass('advance-hidden')) {
+    		$( "#advance-search" ).slideDown('slow');
+    		$( "#advance-search" ).removeClass('advance-hidden');
+    	}
+    	else {
+    		$( "#advance-search" ).addClass('advance-hidden');
+    		$( "#advance-search" ).hide('slow');
+    	}
+		});
+		$( '#advance-search-btn').click(function() {
+			$( "#advance-search" ).addClass('advance-hidden');
+		  $( "#advance-search" ).hide('slow');
+		});
+  });
+
 	// Modal
 	(function ($) {
-	    "use strict";
-	    function centerModal() {
-	        $(this).css('display', 'block');
-	        var $dialog  = $(this).find(".modal-dialog"),
-	        offset       = ($(window).height() - $dialog.height()) / 3,
-	        bottomMargin = parseInt($dialog.css('marginBottom'), 10);
+    "use strict";
+    function centerModal() {
+        $(this).css('display', 'block');
+        var $dialog  = $(this).find(".modal-dialog"),
+        offset       = ($(window).height() - $dialog.height()) / 3,
+        bottomMargin = parseInt($dialog.css('marginBottom'), 10);
 
-	        // Make sure you don't hide the top part of the modal w/ a negative margin if it's longer than the screen height, and keep the margin equal to the bottom margin of the modal
-	        if(offset < bottomMargin) offset = bottomMargin;
-	        $dialog.css("margin-top", offset);
-	    }
+        // Make sure you don't hide the top part of the modal w/ a negative margin if it's longer than the screen height, and keep the margin equal to the bottom margin of the modal
+        if(offset < bottomMargin) offset = bottomMargin;
+        $dialog.css("margin-top", offset);
+    }
 
-	    $(document).on('show.bs.modal', '.modal', centerModal);
-	    $(window).on("resize", function () {
-	        $('.modal:visible').each(centerModal);
-	    });
+    $(document).on('show.bs.modal', '.modal', centerModal);
+    $(window).on("resize", function () {
+        $('.modal:visible').each(centerModal);
+    });
 	}(jQuery));
 });
