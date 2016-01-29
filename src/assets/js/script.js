f2aa944c1dacd82ced910181ccc405244cd593b4
unknown <lee.le@VNSGNICT023.itl.com>
2016-01-29 16:48:12 +0700

    done multi select

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -125,14 +125,24 @@ $(document).ready(function() {
   });
 
   //multiple
-  $('.st-select-box').multiselect({
-     maxHeight: 200,
+  $('#st-select-product-group').multiselect({
+    maxHeight: 200,
     includeSelectAllOption: true,
     enableFiltering: true,
     buttonContainer: '<div class="st-btn-group"></div>',
     buttonClass: 'st-select-toggle',
     templates: {
       button: '<span class="multiselect-selected-text dropdown-toggle" data-toggle="dropdown">Chọn nhóm sản phẩm <i class="fa fa-caret-down"></i></span>'
+    }
+  });
+  $('#st-select-product').multiselect({
+    maxHeight: 200,
+    includeSelectAllOption: true,
+    enableFiltering: true,
+    buttonContainer: '<div class="st-btn-group"></div>',
+    buttonClass: 'st-select-toggle',
+    templates: {
+      button: '<span class="multiselect-selected-text dropdown-toggle" data-toggle="dropdown">Chọn sản phẩm <i class="fa fa-caret-down"></i></span>'
     }
   });
 
