b0da665d2f0f54c65726b8a72c4b27d937621f79
ITL\lee.le <chienbinh.0904@gmail.com>
2016-08-08 18:26:48 +0700

    remove phân trang

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -168,19 +168,24 @@ $(document).ready(function() {
   $(".name-store").keyup(function() {
     $('.link-shop-text').text($('.name-store').val());
   });
-
+    //fanpage-setting
+  $(function () {
+    $( '.setting-form-control').click(function() {
+      $( ".setting-form-db" ).show('fast');
+    });
+  });
 	// Modal
   (function ($) {
     "use strict";
     function centerModal() {
-        $(this).css('display', 'block');
-        var $dialog  = $(this).find(".modal-dialog"),
-        offset       = ($(window).height() - $dialog.height()) / 3,
-        bottomMargin = parseInt($dialog.css('marginBottom'), 10);
+      $(this).css('display', 'block');
+      var $dialog  = $(this).find(".modal-dialog"),
+      offset       = ($(window).height() - $dialog.height()) / 3,
+      bottomMargin = parseInt($dialog.css('marginBottom'), 10);
 
-        // Make sure you don't hide the top part of the modal w/ a negative margin if it's longer than the screen height, and keep the margin equal to the bottom margin of the modal
-        if(offset < bottomMargin) offset = bottomMargin;
-        $dialog.css("margin-top", offset);
+      // Make sure you don't hide the top part of the modal w/ a negative margin if it's longer than the screen height, and keep the margin equal to the bottom margin of the modal
+      if(offset < bottomMargin) offset = bottomMargin;
+      $dialog.css("margin-top", offset);
     }
 
     $(document).on('show.bs.modal', '.modal', centerModal);
